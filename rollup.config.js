f359f4072d1c4553da4ab9562d45775d18872545
lyttonlee <lzr3278@163.com>
2022-12-08 09:59:03 +0800

    change lib name

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,12 @@
  * @Author: lyttonlee lzr3278@163.com
  * @Date: 2022-12-02 13:40:26
  * @LastEditors: lyttonlee lzr3278@163.com
- * @LastEditTime: 2022-12-06 16:14:41
+ * @LastEditTime: 2022-12-08 09:57:33
  * @FilePath: \web-downloader\rollup.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import ts from 'rollup-plugin-typescript2';
-// import { terser } from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 import { defineConfig } from 'rollup';
 import dts from 'rollup-plugin-dts';
 
@@ -16,22 +16,22 @@ const config = defineConfig([
     input: './src/Downloader.ts',
     output: [
       {
-        file: './dist/web-downloader.esm.js',
+        file: './dist/web-file-downloader.esm.js',
         format: 'module',
       },
       {
-        file: './dist/web-downloader.umd.js',
+        file: './dist/web-file-downloader.umd.js',
         format: 'umd',
         name: 'web-downloader',
       },
     ],
     plugins: [
       ts(),
-      // terser({
-      //   compress: {
-      //     drop_console: true,
-      //   },
-      // }),
+      terser({
+        compress: {
+          drop_console: true,
+        },
+      }),
     ],
   },
   {
@@ -39,7 +39,7 @@ const config = defineConfig([
     plugins: [dts()],
     output: {
       format: 'esm',
-      file: 'dist/web-downloader.d.ts',
+      file: 'dist/web-file-downloader.d.ts',
     },
   },
 ]);
